5b399528a26bc71624bc1c86371a1a2d57e6fbc7
KUMARAN TR <53537511+KUMARANTR@users.noreply.github.com>
2020-05-10 02:21:51 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<h1>HELLO WORLD</h1>
+<h1>Hello from KUMARAN TR HTML Page</h1>
+<p>
+<?php
+   echo "Hi Guys.\n";
+   $answer = 5 * 4-1;
+   echo "My Age Is $answer, How are you all ?\n";
+?>
+</p>
+<p>We are good.</p>
